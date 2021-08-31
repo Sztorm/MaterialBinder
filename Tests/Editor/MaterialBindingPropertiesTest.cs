94487eb06e9c3b9e382db83bb1e1fa01fbcb330d
Sztorm <Marcinwicher99@gmail.com>
2021-08-31 16:41:32 +0200

    Added BoolKeywordBinding unit tests.

--- Tests/Editor/MaterialBindingPropertiesTest.cs
+++ Tests/Editor/MaterialBindingPropertiesTest.cs
@@ -8,12 +8,13 @@ using UnityEditor;
 namespace Sztorm.MaterialBinder.Tests
 {
     using static TestUtils;
+    using TestedMatBindings = ShaderGraphs_Graph_TestAllPropsMaterialBindings;
 
     [TestFixture]
     public class MaterialBindingPropertiesTest
     {
         private Material testedMaterial;
-        private ShaderGraphs_Graph_TestAllPropsMaterialBindings testedMaterialBindings;
+        private TestedMatBindings testedMaterialBindings;
 
         [SetUp]
         public void SetUp()
@@ -36,10 +37,12 @@ namespace Sztorm.MaterialBinder.Tests
         {
             (bool V0, float V1, int V2) actual;
             binding.Set(values.V0);
-            binding.Set(values.V1);
-            binding.Set(values.V2);
             actual.V0 = binding.AsBool;
+
+            binding.Set(values.V1);
             actual.V1 = binding.AsFloat;
+
+            binding.Set(values.V2);
             actual.V2 = binding.AsInt;
 
             Assert.AreEqual(expected.V0, actual.V0);
@@ -54,12 +57,15 @@ namespace Sztorm.MaterialBinder.Tests
         {  
             (Vector2 V0, Vector3 V1, Vector4 V2, Color V3) actual;
             binding.Set(values.V0);
-            binding.Set(values.V1);
-            binding.Set(values.V2);
-            binding.Set(values.V3);
             actual.V0 = binding.AsVector2;
+
+            binding.Set(values.V1);
             actual.V1 = binding.AsVector3;
+
+            binding.Set(values.V2);
             actual.V2 = binding.AsVector4;
+
+            binding.Set(values.V3);
             actual.V3 = binding.AsColor;
 
             Assert.That(AreApproximatelyEqual(expected.V0, actual.V0));
@@ -170,6 +176,43 @@ namespace Sztorm.MaterialBinder.Tests
             values.V3 = new Color(1F, 2F, 3F, 4F);
 
             TestVectorBinding(testedMaterialBindings.Color, values, expected);
+        }
+
+        [Test]
+        public void TestBoolKeywordBinding_SetKeyword()
+        {
+            BoolKeywordBinding binding = testedMaterialBindings.BooleanKeyword;
+            bool expected = true;
+            bool value = true;
+
+            binding.SetKeyword(value);
+            bool actual = binding.IsEnabled;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestBoolKeywordBinding_EnableKeyword()
+        {
+            BoolKeywordBinding binding = testedMaterialBindings.BooleanKeyword;
+            bool expected = true;
+
+            binding.EnableKeyword();
+            bool actual = binding.IsEnabled;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestBoolKeywordBinding_DisableKeyword()
+        {
+            BoolKeywordBinding binding = testedMaterialBindings.BooleanKeyword;
+            bool expected = false;
+
+            binding.DisableKeyword();
+            bool actual = binding.IsEnabled;
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
