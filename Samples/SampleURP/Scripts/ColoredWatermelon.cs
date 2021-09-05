0a96528e5a6b812fabc32c2985b3a4b69f035b99
Sztorm <Marcinwicher99@gmail.com>
2021-09-05 02:20:31 +0200

    Removed camera controller script from samples. Added ColoredWatermelon class. Updated sample assets.

---
+++ Samples/SampleURP/Scripts/ColoredWatermelon.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using UnityEngine;
+
+namespace Sztorm.MaterialBinder
+{
+    [RequireComponent(typeof(MeshRenderer))]
+    public sealed class ColoredWatermelon : MonoBehaviour
+    {
+        private const int MinColorCount = 2;
+        private const int MaxColorCount = 10;
+
+        private ShaderGraphs_Graph_WaterMelonMaterialBindings matBindings;
+
+        [SerializeField]
+        private Color[] mainColors;
+
+        [SerializeField]
+        private Color[] additionalColors;
+
+        [SerializeField]
+        private int colorIndex;
+
+        private static void ValidateArray<T>(ref T[] array, int minLength, int maxLength)
+        {
+            if (array == null)
+            {
+                return;
+            }
+            if (array.Length < minLength)
+            {
+                var newArray = new T[minLength];
+
+                array.CopyTo(newArray, index: 0);
+                array = newArray;
+            }
+            else if (array.Length > maxLength)
+            {
+                var newArray = new T[maxLength];
+
+                Array.Copy(
+                    sourceArray: array,
+                    sourceIndex: 0,
+                    destinationArray: newArray,
+                    length: maxLength,
+                    destinationIndex: 0);
+
+                array = newArray;
+            }
+        }
+
+        private void OnValidate()
+        {
+            ValidateArray(ref mainColors, MinColorCount, MaxColorCount);
+            ValidateArray(
+                ref additionalColors, minLength: mainColors.Length, maxLength: mainColors.Length);
+
+            colorIndex %= mainColors.Length;
+        }
+
+        private void Awake()
+        {
+            var renderer = GetComponent<MeshRenderer>();
+            Material material = renderer.material;
+
+            matBindings.Bind(material);
+        }
+
+        private void OnMouseDown()
+        {
+            Color mainColor = mainColors[colorIndex];
+            Color additionalColor = additionalColors[colorIndex];
+
+            matBindings.MainColor.Set(mainColor);
+            matBindings.AdditionalColor.Set(additionalColor);
+
+            colorIndex = (colorIndex + 1) % mainColors.Length;
+        }
+    }
+}
